7b6f70c911bd2c39af572aa3eaa6ae9557d88f21
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-05-11 10:22:03 +0200

    07trsf corrections

--- src/texts/07_audit_dashboard.sql
+++ src/texts/07_audit_dashboard.sql
@@ -1,24 +1,18 @@
 set ref_date = (select max("date") from "shop_03_complete")
 ;
 --next_querry
-
 set sleva_hranice = 3
 ;
 --next_querry
-
 set tolerance = 3
 ;
 --next_querry
-
-
 set aktualizace = (select left(max("date"),16) from "shop_raw")
 ;
 --next_querry
-
 set days_back = 30
 ;
 --next_querry
-
 create or replace table "shop_current" as
 select "shop"
     , "itemId"
@@ -37,7 +31,6 @@ from "shop_03_complete"
 where "date" = $ref_date
 ;
 --next_querry
-
 create or replace table "shop_30d" as
 select "shop"
     , "itemId"
@@ -56,7 +49,6 @@ from "shop_03_complete"
 where "date" >= DATEADD("d", - $days_back, $ref_date)
 ;
 --next_querry
-
 create or replace table "shop_HS_differences" as
 select *
 from "shop_current"
@@ -65,7 +57,6 @@ where "sleva_dle_shopu" != try_to_number("sleva_dle_Hlidace",2) and abs("sleva_d
 order by "sleva_dle_shopu" desc
 ;
 --next_querry
-
 create or replace table "count_items" as
 select distinct("shop")
     , count(distinct("itemId")) over () as "Produktu_celkem" --"count_clean"
@@ -73,29 +64,36 @@ select distinct("shop")
 from "shop_current"
 ;
 --next_querry
-
 create or replace table "count_disc_shop" as
-select distinct(count(distinct("itemId")) over ()) as "Produktu_ve_sleve_(shop)"--"count_bf"
-from "shop_current"
-having "sleva_dle_shopu" > $sleva_hranice
+select 
+    case
+        when count("itemId") is null then 0
+        else count(distinct("itemId"))
+    end as "Produktu_ve_sleve_(shop)"--"count_bf"
+from (select * from "shop_current" where to_number("sleva_dle_shopu",12,2) > $sleva_hranice)
 ;
 --next_querry
-
 create or replace table "count_disc_HS" as
-select distinct(count(distinct("itemId")) over ()) as "Produktu_ve_sleve_(HS)"
-from "shop_current"
-having try_to_number("sleva_dle_Hlidace",2) > $sleva_hranice
+select case
+        when count("itemId") is null then 0
+        else count(distinct("itemId"))
+    end as "Produktu_ve_sleve_(HS)"
+from (select * from "shop_current" where try_to_number("sleva_dle_Hlidace",2) > $sleva_hranice)
 ;
 --next_querry
-
 create or replace table "count_diff" as
-select count(distinct("itemId")) over () as "Neshoda_ve_sleve_(all)"
+select case
+        when count("itemId") is null then null
+        else count(distinct("itemId"))
+    end as "Neshoda_ve_sleve_(all)"
 from "shop_HS_differences"
 ;
 --next_querry
-
 create or replace table "count_diff_shop-only" as
-select distinct(count(distinct("itemId")) over ()) as "Neshoda_ve_sleve"
+select case
+        when count("itemId") = 0 then null
+        else count(distinct("itemId"))
+    end as "Neshoda_ve_sleve"
 from 
 (    select "diff".*
     from "shop_HS_differences" "diff"
@@ -105,21 +103,17 @@ from
 )
 ;
 --next_querry
-
 create or replace table "avg_disc_shop" as
-select round(avg("sleva_dle_shopu") over (),2) as "Prumerna_uvadena_sleva"
-from "shop_current"
-having "sleva_dle_shopu" > $sleva_hranice
+select distinct(round(avg("sleva_dle_shopu") over (),2)) as "Prumerna_uvadena_sleva"
+      from "shop_current" where to_number("sleva_dle_shopu",12,2) > $sleva_hranice
 ;
 --next_querry
-
 create or replace table "avg_disc_HS" as
-select round(avg(try_to_number("sleva_dle_Hlidace",12,2)) over (),2) as "Prumerna_realna_sleva"
+select distinct(round(avg(try_to_number("sleva_dle_Hlidace",12,2)) over (),2)) as "Prumerna_realna_sleva"
 from "shop_current"
 having "sleva_dle_shopu" > $sleva_hranice
 ;
 --next_querry
-
 // spreadsheet - 1 záložka všechny neshody + označení typu neshody
 create or replace table "shop_false_discounts" as
 select case
@@ -136,7 +130,6 @@ end as "typ_neshody"
 order by 1 asc
 ;
 --next_querry
-
 // spreadsheet - 2 záložka navýšené originalPrice (za posledních 30 dní)
 // omezeno na: změna o víc jak 3% a pouze tam, kde se neshodneme na slevě
 create or replace table "shop_incr_origPrice" as
@@ -167,33 +160,27 @@ qualify "row_num" = 1 and "incr" = 'incr'
     and abs("sleva_dle_shopu" - try_to_number("sleva_dle_Hlidace",2)) >= $tolerance
 ;
 --next_querry
-
-alter table "shop_incr_origPrice" drop column "row_num";
+alter table "shop_incr_origPrice" drop column "row_num"
+;
 --next_querry
-
 create or replace table "count_incr_origPrice" as
-select count(distinct("itemId")) as "Produktu_s_navysenou_refCenou"
+select case
+        when count("itemId") = 0 then null
+        else count(distinct("itemId"))
+    end as "Produktu_s_navysenou_refCenou"
 from "shop_incr_origPrice"
 ;
 --next_querry
-
 create or replace table "shop_dashboard" as
 select distinct("shop"."shop") as "Shop"
     , "shop"."Produktu_celkem" as "Produktu_celkem" -- celkový počet produktů shopu k poslednímu dni
     , "disc_shop"."Produktu_ve_sleve_(shop)" as "Produktu_ve_sleve" -- počet produktů ve slevě dle shopu (hranice slevy viz $sleva_hranice)
-    --, "disc_HS"."Produktu_ve_sleve_(HS)" as "Produktu_ve_sleve_(HS)"
-    , "diff_shop"."Neshoda_ve_sleve" as "Produktu_s_chybnou_slevou" -- produkty dle shopu ve slevě, kde se s HS neshodne o 3 a více %
---    , "diff".*
-    , "incr_orig".* -- produkty s navýšenou originalPrice během posledních 30 dní
-    , "avg_shop".*
-    , "avg_HS".*
+    , iff("Produktu_ve_sleve" = 0, null, (select * from "count_diff_shop-only")) as "Produktu_s_chybnou_slevou" -- produkty dle shopu ve slevě, kde se s HS neshodne o 3 a více %
+    , "incr_orig"."Produktu_s_navysenou_refCenou" as "Produktu_s_navysenou_refCenou" -- produkty s navýšenou originalPrice během posledních 30 dní
+    , iff("Produktu_ve_sleve" = 0, null, (select * from "avg_disc_shop")) as "Prumerna_uvadena_sleva" -- průměrná sleva uváděná shopem
+    , iff("Produktu_ve_sleve" = 0, null, (select * from "avg_disc_HS")) as "Prumerna_realna_sleva" -- průměrná sleva podle Hlídače (na produktech vedených shopem jako zlevněné)
     , $aktualizace as "Aktualizace"
 from "count_items" "shop"
 left join "count_disc_shop" "disc_shop"
-left join "count_disc_HS" "disc_HS"
---left join "count_diff" "diff"
-left join "count_diff_shop-only" "diff_shop"
-left join "avg_disc_shop" "avg_shop"
-left join "avg_disc_HS" "avg_HS"
 left join "count_incr_origPrice" "incr_orig"
 ;
