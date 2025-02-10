ddcb7ecc4d76110e63fcf7cc482d2d5c904da03e
MartinaGelnerova <82106162+MartinaGelnerova@users.noreply.github.com>
2025-02-10 11:01:12 +0100

    Update storage.js
    Changing rooting to GCP

--- src/storage.js
+++ src/storage.js
@@ -5,7 +5,7 @@ export async function getOrCreateTable (shopName, KEBOOLA_TOKEN) {
     console.log(`Checking if table ${shopName} already exists.`)
 
     const getUrl =
-        'https://connection.eu-central-1.keboola.com/v2/storage/tables?include=buckets,columns,metadata,columnMetadata'
+        'https://connection.europe-west3.gcp.keboola.com/v2/storage/tables?include=buckets,columns,metadata,columnMetadata'
     const getMethod = 'GET'
     const getHeaders = {
         'x-storageapi-token': KEBOOLA_TOKEN
@@ -37,7 +37,7 @@ export async function getOrCreateTable (shopName, KEBOOLA_TOKEN) {
     // Not sure if dataFileId will last forever, hope so, otherwise either get new Id by manually uploading the csv file, or contact Keboola how to do it
 
     const postUrl =
-        'https://connection.eu-central-1.keboola.com/v2/storage/buckets/in.c-black-friday/tables-async'
+        'https://connection.europe-west3.gcp.keboola.com/v2/storage/buckets/in.c-black-friday/tables-async'
     const postMethod = 'POST'
     const postHeaders = {
         'content-type': 'application/x-www-form-urlencoded',
@@ -69,7 +69,7 @@ export async function getTables (KEBOOLA_TOKEN) {
     console.log(`Getting information about all tables`)
 
     const getUrl =
-        'https://connection.eu-central-1.keboola.com/v2/storage/tables?include=buckets,columns,metadata,columnMetadata'
+        'https://connection.europe-west3.gcp.keboola.com/v2/storage/tables?include=buckets,columns,metadata,columnMetadata'
     const getMethod = 'GET'
     const getHeaders = {
         'x-storageapi-token': KEBOOLA_TOKEN
